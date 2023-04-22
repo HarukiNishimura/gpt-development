b252badfa58b7e37deeab05b0bc4117b40b1fdba
HarukiNishimura <k859375@gmail.com>
2023-04-22 19:42:12 +0900

    tesuto

--- gpt-laravel-project/resources/views/welcome.blade.php
+++ gpt-laravel-project/resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                welcome
+                welcomewwww
             </div>
 
             <div class="links">
